eba35f3e49031cfdeca1c40ca30472855e5f3688
basith-04 <basithofficialmail01@gmail.com>
2025-12-06 11:42:20 +0530

    add color picker and scheme selection functionality

---
+++ color-scheme-generator/index.js
@@ -0,0 +1,9 @@
+const getBtn = document.getElementById('get-btn');
+const colorScheme = document.getElementById('color-scheme');
+const colorPicker = document.getElementById('color-picker');
+const schemeSelect = document.getElementById('scheme-select');
+
+getBtn.addEventListener('click', () => {
+    console.log(colorPicker.value)
+console.log(schemeSelect.value)
+});
